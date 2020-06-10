7d43ca2759175192dbb56f78d59865c675d6dbae
Júlio Alves <julio619alves@gmail.com>
2020-06-10 17:22:11 -0300

    implement new imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import {View, StyleSheet} from 'react-native';
+
 import Simple from './components/Simple';
 import EvenOdd from './components/EvenOdd';
+import {Reverse, MegaSena} from './components/Multi';
 
 export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Simple text='Hello World!'></Simple>
-				<EvenOdd number={223}></EvenOdd>
+				<Simple text='Hello World!'/>
+				<EvenOdd number={223}/>
+				<Reverse text='React Native'/>
+				<MegaSena/>
 			</View>
 		)
 	}
--- src/components/EvenOdd.js
+++ src/components/EvenOdd.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import Simple from '../components/Simple'
+import Template from '../style/Template'
 
 export default props => 
     <View>
-        {props.number % 2 == 0 ? <Text style={Simple.ex}>{props.number} é número par!</Text> : <Text style={Simple.ex}>{props.number} é número ímpar!</Text>}
+        {props.number % 2 == 0 ? <Text style={Template.ex}>{props.number} é número par!</Text> : <Text style={Template.ex}>{props.number} é número ímpar!</Text>}
     </View>
