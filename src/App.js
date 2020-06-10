72f6bdefbd7f0dfcb439eeec588567acb9941657
Júlio Alves <julio619alves@gmail.com>
2020-06-10 16:55:08 -0300

    add even or odd - conditional render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import {View, StyleSheet} from 'react-native';
 import Simple from './components/Simple';
+import EvenOdd from './components/EvenOdd';
 
 export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
 				<Simple text='Hello World!'></Simple>
+				<EvenOdd number={223}></EvenOdd>
 			</View>
 		)
 	}
--- src/components/EvenOdd.js
+++ src/components/EvenOdd.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Simple from '../components/Simple'
+
+export default props => 
+    <View>
+        {props.number % 2 == 0 ? <Text style={Simple.ex}>{props.number} é número par!</Text> : <Text style={Simple.ex}>{props.number} é número ímpar!</Text>}
+    </View>
